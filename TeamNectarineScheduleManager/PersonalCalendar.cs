5332ac3bfcf2f94c05af3e252890213adeef9178
FilipPetkov <filip.petkov@gmail.com>
2016-12-26 17:44:25 +0200

    update

---
+++ TeamNectarineScheduleManager/PersonalCalendar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamNectarineScheduleManager
+{
+    class PersonalCalendar : Calendar
+    {
+        private const int DaysLeave = 20; // this is how many days of paid leave every employee has
+        private DateTime[] plannedLeave; // days with planned leave
+
+        public PersonalCalendar(DateTime[] plannedLeave)
+        {
+            this.plannedLeave = plannedLeave;
+        }
+    }
+}
---
+++ TeamNectarineScheduleManager/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamNectarineScheduleManager
+{
+    public class User
+    {
+        private string username; // needed for log-in
+        private string password; // needed for log-in
+    }
+}
