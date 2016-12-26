43c921b03ff34c5e01c49b259e40ff3bb8db868b
FilipPetkov <filip.petkov@gmail.com>
2016-12-26 17:11:53 +0200

    Add 2 classes
    DailyEvents and Calendar

--- TeamNectarineScheduleManager/Calendar.cs
+++ TeamNectarineScheduleManager/Calendar.cs
@@ -6,10 +6,51 @@ using System.Threading.Tasks;
 
 namespace TeamNectarineScheduleManager
 {
-    class Calendar
+    public enum DayOfWeek
     {
-        private Month month;
-        private int week; // specify week number
-        private Day day;
+        Monday = 1,
+        Tuesday,
+        Wednsday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
     }
+    class Calendar : DailyEvents
+    {
+        private Month _month;
+        private int _week; // specify week number, must be between 1 and 53 inclusive
+        private DayOfWeek _day;
+        public List<DailyEvents> _employeeDailyEvents;//Creates list of with events for the current calendar.
+        public Calendar(Month month, int week, DayOfWeek day)
+        {
+            this._month = month;
+            this._day = day;
+            this._week = week;
+            _employeeDailyEvents = new List<DailyEvents>();
+        }
+               public List<DailyEvents> CalendarEvents
+        {
+            get
+            {
+                return _employeeDailyEvents;
+            }
+            set
+            {
+                _employeeDailyEvents = value;
+            }
+        }
+        public List<DailyEvents> AddEvent(DailyEvents currentEvent, Calendar calendar)
+        {
+            calendar.CalendarEvents.Add(currentEvent);
+            return CalendarEvents;
+        }//Adds event for the current caledar
+        public List<DailyEvents> RemoveEvent(int removeIndex,Calendar calendar)
+        {
+            calendar.CalendarEvents.RemoveAt(removeIndex);
+            return CalendarEvents;
+        }//Removes event from the current calendar
+    }
+   
 }
+
